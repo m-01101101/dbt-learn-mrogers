43191aa7c0fd306d3828f5c9ebb52e9b198bc51b
michael <michaelrogers.arsenal@gmail.com>
2020-05-13 13:11:54 +0100

    Adding lifetime spend to customers table

--- models/customers.sql
+++ models/customers.sql
@@ -9,27 +9,29 @@
     )
     
     , orders as (
-        select * from {{ ref('stg_orders') }}
+        select * from {{ ref('orders') }}
     )
     
     , customer_orders as (
         select
-            customer_id,
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            customer_id
+            , min(order_date) first_order_date
+            , max(order_date) most_recent_order_date
+            , count(order_id) number_of_orders
+            , sum(amount) lifetime_spend
         from orders
         group by 1
     )
     
     , final as (
         select
-            customers.customer_id,
-            customers.first_name,
-            customers.last_name,
-            customer_orders.first_order_date,
-            customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+            customers.customer_id
+            , customers.first_name
+            , customers.last_name
+            , customer_orders.first_order_date
+            , customer_orders.most_recent_order_date
+            , coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+            , coalesce(customer_orders.lifetime_spend, 0) as lifetime_spend
         from customers
             left join customer_orders using (customer_id) 
     )
