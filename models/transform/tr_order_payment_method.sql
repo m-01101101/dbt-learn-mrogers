30ae0583fb46554af55a062fb0fba29cf1c59b6c
michael <michaelrogers.arsenal@gmail.com>
2020-05-14 14:54:40 +0100

    tidying up jinja

--- models/transform/tr_order_payment_method.sql
+++ models/transform/tr_order_payment_method.sql
@@ -1,10 +1,15 @@
-{%- set payment_methods = ["bank_transfer", "credit_card", "gift_card"] -%}
+  
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'gift_card', 'coupon'] -%}
 
 select
-    order_id
+    orderid
     {%- for payment_method in payment_methods %}
-    , sum(case when payment_method = '{{payment_method}}' then amount end) {{payment_method}}_amount
+    , sum(case when paymentmethod = '{{payment_method}}' then amount end) {{payment_method}}_amount
     {% endfor %}
+    {#-
+        you can do something like "{% ',' if not loop.last %}" or "{%- if not loop.last %},{% endif -%}" 
+        if using trailing commas
+    -#}
     , sum(amount) total_amount
-from {{ ref('raw_payments') }}
+from {{ ref('stg_stripe_payments') }}
 group by 1
